3a57c2478b8e157716c006ffcebb484c9fa19310
Tsai <czn826@gmail.com>
2021-10-04 17:59:52 +1100

    update launchDetailPage

--- src/components/launchDetailPage/LaunchDetailPage.js
+++ src/components/launchDetailPage/LaunchDetailPage.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { VscArrowLeft, VscArrowRight } from "react-icons/vsc";
+
+import './LaunchDetailPage.css'
 
 const LaunchDetailPage = (props) => {
   const launchesData = props.launches;
   const coresData = props.cores;
   const number = props.match.params.number - 1;
+  const singleLaunchData = launchesData[number]
   console.log(number)
 
   return (
-    <div>
+    <div className='launchDetail'>
       <Link to='/launches'>
-        <button>Back</button>
+        <div className='launchDetail-backButton'>
+          <VscArrowLeft />
+        </div>
       </Link>
       <div>
-        {launchesData[number].name}
+        <Link to={`/launch/${number}`}><VscArrowLeft /></Link>
+        <Link to={`/launch/${number+2}`}><VscArrowRight /></Link>
+      </div>
+      <div>
+        {singleLaunchData.name}
+      </div>
+      <div>
+        <img src={singleLaunchData.links.patch.small} alt="" />
       </div>
 
     </div>
