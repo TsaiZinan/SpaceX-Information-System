1215535257f2ead8c23de2159511870bc090c88e
Tsai <czn826@gmail.com>
2021-10-05 17:04:26 +1100

    singleLaunchPage Style Update

--- src/components/launchDetailPage/LaunchDetailPage.js
+++ src/components/launchDetailPage/LaunchDetailPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { VscArrowLeft, VscArrowRight } from "react-icons/vsc";
 
@@ -16,24 +16,72 @@ const LaunchDetailPage = (props) => {
 
   return (
     <div className='launchDetail'>
-      <Link to='/launches'>
-        <div className='launchDetail-backButton'>
-          <VscArrowLeft />
+
+      <div className='launchDetail-controlPanal'>
+        <div>
+          <Link to='/launches'>
+            <VscArrowLeft className='launchDetail-Button' />
+          </Link>
         </div>
-      </Link>
-      <div>
-        
-        <Link to={`/launch/${launchNumber == 1 ? launchDataLength : launchNumber - 1}`}><VscArrowLeft className='launchDetail-backButton'/></Link>
-        
-        
-        <Link to={`/launch/${launchNumber == launchDataLength ? 1 : Number(launchNumber) + 1}`}><VscArrowRight className='launchDetail-backButton'/></Link>
+
+        <div className='launchDetail-controlPanal-page'>
+
+          <Link to={`/launch/${launchNumber == 1 ? launchDataLength : launchNumber - 1}`}>
+            <VscArrowLeft className='launchDetail-Button' />
+          </Link>
+
+
+          <Link to={`/launch/${launchNumber == launchDataLength ? 1 : Number(launchNumber) + 1}`}>
+            <VscArrowRight className='launchDetail-Button' />
+          </Link>
+        </div>
       </div>
-      <div>
-        {singleLaunchData.name}
+
+      <div className='launchDetail-first'>
+
+        <iframe
+          className='launchDetail-embedVideo'
+          width="560" 
+          height="315"
+          src={`https://www.youtube.com/embed/${singleLaunchData.links.youtube_id}`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+
+        <div className='launchDetail-basicInfo'>
+          <div className='launchDetail-basicInfo-launch'>
+            <div className='launchDetail-basicInfo-launch-numpatch'>
+              <div>
+                <img style={{ width: '100px', paddingLeft: '20px' }} src={singleLaunchData.links.patch.small} alt="" />  
+              </div>
+              <div style={{ fontSize: '90px', paddingRight: '30px', marginTop: '-10px' }}>{singleLaunchData.flight_number}</div>
+            </div>
+            <div>{singleLaunchData.name}</div>
+            <div>{singleLaunchData.date_utc}</div>
+            <div>{singleLaunchData.launchpad}</div>
+          </div>
+
+
+
+          <div className='launchDetail-basicInfo-core'>
+            <div>First Stage:{singleLaunchData.cores[0].core}</div>
+            <div>Reused Times: {singleLaunchData.cores[0].flight}</div>
+            <div>Landing Type: {singleLaunchData.cores[0].landing_type}</div>
+            <div>Landing Site: {singleLaunchData.cores[0].landpad}</div>
+            <div>Landing Result: {singleLaunchData.cores[0].landing_success === true? 'Y' : 'N'}</div>
+          </div>
+        </div>
       </div>
-      <div>
-        {singleLaunchData.flight_number}
-        <img src={singleLaunchData.links.patch.small} alt="" />
+
+
+      <div className='launchDetail-second'>
+        {singleLaunchData.details}
+      </div>
+      
+      <div style={{ height: '50px' }}>
+        {' '}
       </div>
 
     </div>
