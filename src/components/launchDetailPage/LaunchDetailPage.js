1cc4ec838a54f4c8efacdce4c030c439e193d107
Tsai <czn826@gmail.com>
2021-10-07 04:55:41 +1100

    launchdetailpage style update

--- src/components/launchDetailPage/LaunchDetailPage.js
+++ src/components/launchDetailPage/LaunchDetailPage.js
@@ -41,8 +41,8 @@ const LaunchDetailPage = (props) => {
 
         <iframe
           className='launchDetail-embedVideo'
-          width="560" 
-          height="315"
+          // width="560" 
+          // height="315"
           src={`https://www.youtube.com/embed/${singleLaunchData.links.youtube_id}`}
           title="YouTube video player"
           frameborder="0"
@@ -53,25 +53,31 @@ const LaunchDetailPage = (props) => {
         <div className='launchDetail-basicInfo'>
           <div className='launchDetail-basicInfo-launch'>
             <div className='launchDetail-basicInfo-launch-numpatch'>
-              <div>
-                <img style={{ width: '100px', paddingLeft: '20px' }} src={singleLaunchData.links.patch.small} alt="" />  
+
+              <div className='launchDetail-basicInfo-launch-patch'>
+                <img className='launchDetail-basicInfo-launch-patch-img' src={singleLaunchData.links.patch.small} alt="" />
               </div>
-              <div style={{ fontSize: '90px', paddingRight: '30px', marginTop: '-10px' }}>{singleLaunchData.flight_number}</div>
+              {/* <div>{singleLaunchData.flight_number}</div> */}
+              <div className='launchDetail-basicInfo-launchTimeSite'>
+                
+                {/* <div>{singleLaunchData.launchpad}</div> */}
+                <div>{singleLaunchData.date_utc.substring(0, 10)}</div>
+              </div>
+
+
             </div>
-            <div>{singleLaunchData.name}</div>
-            <div>{singleLaunchData.date_utc}</div>
-            <div>{singleLaunchData.launchpad}</div>
+            <div className='launchDetail-basicInfo-name'>{singleLaunchData.name}</div>
+            {/* <div>{singleLaunchData.date_utc}</div> */}
+            {/* <div>{singleLaunchData.launchpad}</div> */}
           </div>
+        </div>
 
-
-
-          <div className='launchDetail-basicInfo-core'>
-            <div>First Stage:{singleLaunchData.cores[0].core}</div>
-            <div>Reused Times: {singleLaunchData.cores[0].flight}</div>
-            <div>Landing Type: {singleLaunchData.cores[0].landing_type}</div>
-            <div>Landing Site: {singleLaunchData.cores[0].landpad}</div>
-            <div>Landing Result: {singleLaunchData.cores[0].landing_success === true? 'Y' : 'N'}</div>
-          </div>
+        <div className='launchDetail-basicInfo-core'>
+          <div>First Stage:{singleLaunchData.cores[0].core}</div>
+          <div>Reused Times: {singleLaunchData.cores[0].flight}</div>
+          <div>Landing Type: {singleLaunchData.cores[0].landing_type}</div>
+          <div>Landing Site: {singleLaunchData.cores[0].landpad}</div>
+          <div>Landing Result: {singleLaunchData.cores[0].landing_success === true ? 'Y' : 'N'}</div>
         </div>
       </div>
 
@@ -79,7 +85,7 @@ const LaunchDetailPage = (props) => {
       <div className='launchDetail-second'>
         {singleLaunchData.details}
       </div>
-      
+
       <div style={{ height: '50px' }}>
         {' '}
       </div>
