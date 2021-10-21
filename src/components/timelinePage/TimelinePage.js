bbc8529f8f15ff82bb4e4d02b0486405432cec8d
Jack Zhou <jackmkj@gmail.com>
2021-10-21 13:04:29 +1100

    final timeline

--- src/components/timelinePage/TimelinePage.js
+++ src/components/timelinePage/TimelinePage.js
@@ -1,114 +1,99 @@
 import React, { Component } from 'react'
 import { ReactComponent as RocketIcon } from './rocket.svg'
-import {VerticalTimeline,VerticalTimelineElement} from "react-vertical-timeline-component"
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css";
 import './TimelinePage.css'
 import { SiReddit, SiYoutube, SiWikipedia, SiSpacex } from "react-icons/si";
 
+import { LatestLaunch, Cores, AllLaunches } from '../../data'
 
-let timelineElements = [
-  {
-    id: 1,
-    title: "Launch 1",
-    location: "Dragontail, Ascana",
-    description:
-      "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
-    buttonText: "Wikipedia",
-    date: " 2016 - present",
-    icon: "RocketIcon",
-  },
-  {
-    id: 2,
-    title: "Launch 2",
-    location: "Skystead, Craonia",
-    description:
-      "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
-    buttonText: "Wikipedia",
-    date: "June 2013 - August 2016",
-    icon: "RocketIcon",
-  },
-  {
-    id: 3,
-    title: "Launch 3",
-    location: "South Warren, Geshington",
-    description:
-      "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
-    buttonText: "Wikipedia",
-    date: "September 2011 - June 2013",
-    icon: "RocketIcon",
-  },
-  {
-    id: 4,
-    title: "Launch 4",
-    location: "South Warren, Geshington",
-    description:
-      "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-    buttonText: "Wikipedia",
-    date: "September 2011",
-    icon: "RocketIcon",
-  },
 
-];
+// const rocketInfo = AllLaunches
+
+// const aaa = AllLaunches[0]
+
+// console.log(aaa.links.patch.small)
 
 
 
-let RocketIconStyles = {background: "#06D6A0"};
-function TimelinePage () {
+
+let RocketIconStyles = { background: "#06D6A0" };
+function TimelinePage() {
   return (
     <div className='TimelinePage'>
-     <div className = 'PageTitle'>
-       <h1>TIMELINE</h1>
-       </div> 
-     <VerticalTimeline>
-       {
-         timelineElements.map((element) =>{
-           let showButton = element.buttonText !== undefined && 
-           element.buttonText !== null && 
-           element.buttonText !== "";
-           
-           return(
-               <VerticalTimelineElement
-                key={element.key}
-                date={element.date}
+      <div className='PageTitle'>
+        <h1>TIMELINE</h1>
+
+
+        {/* {AllLaunches.map((singleLaunch) => {
+          console.log(singleLaunch.links.patch.small)
+          return(
+            <div>
+              <img src={singleLaunch.links.patch.small} alt="" />
+              <p>{singleLaunch.name}</p>
+            {console.log(singleLaunch.name)}
+            </div>
+          )
+        })} */}
+
+      </div>
+
+      <VerticalTimeline>
+        {
+          AllLaunches.map((singleLaunch) => {
+            // let showButton = element.buttonText !== undefined &&
+            //   element.buttonText !== null &&
+            //   element.buttonText !== "";
+
+            return (
+              <VerticalTimelineElement
+                key={singleLaunch.flight_number}
+                date={singleLaunch.date_utc}
                 dateClassName="date"
-                iconStyle={RocketIconStyles}        
-                icon={<RocketIcon/>}
-               >
-
-              <h3 className = "vertical-timeline-element-title">
-              {element.title} 
-              </h3>
-
-              <h5 className = "vertical-timeline-element-subtitle">
-                {element.location}
-              </h5>
-
-              <p id ="description">
-                {element.description}
-              </p>
-
-        
-              {showButton && 
-              <a 
-               className ="Button"
-               href="/"
+                iconStyle={RocketIconStyles}
+                icon={<RocketIcon />}
               >
-              {element.buttonText}</a>}
-              
+                <img src={singleLaunch.links.patch.small} alt="" />
+                <h3 className="vertical-timeline-element-title">
+                  {singleLaunch.name}
+                </h3>
 
-           {/*{showButton && 
-          <a href={wikipedia}>
-          <SiWikipedia className='launch-hoverBox-link' />
-          </a>}*/}
+                <h5 className="vertical-timeline-element-subtitle">
+                  {singleLaunch.launchpad}
+                </h5>
 
-               </VerticalTimelineElement>
-           )
-         })
-       }
+                <p id="description">
+                  {singleLaunch.details}
+                </p>
 
-     </VerticalTimeline>
 
-    
+                <a
+                  className="Button"
+                  href={singleLaunch.links.webcast}
+                >  <SiYoutube />
+                  {/* {element.buttonText} */}
+                </a>
+                {/* <a href="https://www.google.com"><SiWikipedia /></a>
+               */}
+                <a
+                  className="Button"
+                  href={singleLaunch.links.wikipedia}
+                >  <SiWikipedia />
+                  {/* {element.buttonText} */}
+                </a>
+     
+
+
+
+              </VerticalTimelineElement>
+            )
+          })
+        }
+
+      </VerticalTimeline>
+
+
+
     </div>
   );
 }
