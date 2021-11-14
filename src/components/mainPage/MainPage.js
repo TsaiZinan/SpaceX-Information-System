b5e894e7bb45a71f219b375a66b9a707655284b8
Tsai <czn826@gmail.com>
2021-11-15 05:36:16 +1100

    mainPage launchBlock grid done

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -10,17 +10,26 @@ const MainPage = (props) => {
   const AllLaunches = props.launches;
   const LaunchPads = props.launchpads;
 
-  const LaunchBlock = () => {
-    return(
+  const LaunchBlock = (props) => {
+    let blockUnit = (name, content) => {
+      return (
+        <div className='launch-block-m-blockunit'>
+          <div className='launch-block-m-blockunit-name'>{name}</div>
+          <div className='launch-block-m-blockunit-content'>{content}</div>
+        </div>
+      )
+    }
+
+    return (
       <div className='launch-block-m'>
         <div className='launch-block-m-patch'>
           <img className='launch-block-m-patch-img' src="https://i.imgur.com/kIHwGnk.png" alt="" />
         </div>
 
         <div className='launch-block-m-title'>
-          Next Launch
+          {props.mode === 'next' ? 'Next Launch' : 'Last Launch'}
         </div>
-        
+
         <div className='launch-block-m-name'>
           Crew-3
         </div>
@@ -28,12 +37,24 @@ const MainPage = (props) => {
         <div className='launch-block-m-time'>
           2021-11-11 08:56
         </div>
+
+        <div className='blockunit-row'>
+          {blockUnit('First Stage:', 'B1058.9')}
+          {blockUnit('Launch Site:', 'SLC-4E')}
+        </div>
+
+        <div className='blockunit-row'>
+          {blockUnit('Landing Site:', 'Of Course I Still Love You')}
+          {props.mode === 'last' ? blockUnit('Landing Result:', 'Success') : null}
+          
+        </div>
+
       </div>
     )
   }
 
   const SmallBlock = (props) => {
-    return(
+    return (
       <div className='launch-block-s'>
         <div className='launch-block-s-title'>
           {props.title}
@@ -45,39 +66,40 @@ const MainPage = (props) => {
     )
   }
 
-  let content = <div class="lds-roller">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
+  let content =
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
 
   if (AllLaunches.length > 1 && LaunchPads.length > 1 && Object.keys(LatestLaunch).length != 0) {
     content =
       <div className='mainpage'>
         <div className='mainpage-block'>
-          <LaunchBlock />
+          <LaunchBlock mode='next'/>
         </div>
         <div className='mainpage-block'>
-          <LaunchBlock />
+          <LaunchBlock mode='last'/>
         </div>
         <div className='mainpage-block'>
-          <SmallBlock title='Launch' number='137'/>
-          <SmallBlock title='Launch this Year' number='20'/>
-          <SmallBlock title='Success' number='135'/>
-          <SmallBlock title='Recover' number='62'/>
+          <SmallBlock title='Launch' number='137' />
+          <SmallBlock title='Launch this Year' number='20' />
+          <SmallBlock title='Success' number='135' />
+          <SmallBlock title='Recover' number='62' />
         </div>
-        
+
         <div className='mainpage-block'>
           <div className='mainpage-block-chart'>
             <LaunchChart data={AllLaunches} latest={LatestLaunch} type='month' />
           </div>
         </div>
-        
+
         <div className='mainpage-block'>
           <div className='mainpage-block-chart'>
             <LaunchChart data={AllLaunches} latest={LatestLaunch} type='year' />
